d427955c95676fe2de91a623271919388478fdd8
ghanizadev <jeanmelo@ghanizadev.com.br>
2020-01-07 02:36:11 -0300

    button + flex flow

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import {Container, Content, Form, Item, Label, Icon, Input} from 'native-base';
+import {Container, Content, Form, Item, Label, Icon, Input, View, Text, Button} from 'native-base';
 import {Image} from 'react-native';
 import Logo from '../../assets/images/logo.png';
 
 const LoginForm = props => {
     
     return (
-        <Container>
+        <Container style={{paddingVertical: 50, paddingHorizontal: 30}}>
         <Image source={require('../../assets/images/logo.png')}
         style={{
             height: '25%',
             width: '75%',
-            alignSelf: 'center'
+            alignSelf: 'center',
+            flex: 1
         }}
-        resizeMode="cover" />
+        resizeMode="contain" />
             <Content>
                 <Form>
                 <Item floatingLabel>
@@ -21,13 +22,29 @@ const LoginForm = props => {
                     <Label>E-mail</Label>
                     <Input />
                 </Item>
-                <Item floatingLabel>
+                <Item floatingLabel last>
                     <Icon name="ios-key" />
                     <Label>Senha</Label>
                     <Input secureTextEntry />
                 </Item>
                 </Form>
             </Content>
+            <View
+            style={{
+                flex: 1
+            }}>
+            <Button
+            rounded
+            style={{
+                justifyContent: 'center',
+                alignSelf: 'center',
+                backgroundColor: '#FFBC2B'
+            }}>
+                <Text>
+                    Entrar
+                </Text>
+            </Button>
+            </View>
         </Container>
     );
 }
